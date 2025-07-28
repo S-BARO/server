7d351d34830c09f629b1aeba5890fe1e93da0a8f
codrin2 <hssang127@gmail.com>
2025-07-28 17:57:10 +0900

    feat: 사용자 정보를 관리하는 Member 엔티티 구현 #1

---
+++ src/main/kotlin/com/dh/baro/identity/domain/Member.kt
@@ -0,0 +1,33 @@
+package com.dh.baro.identity.domain
+
+import com.dh.baro.core.AbstractTime
+import com.dh.baro.core.converter.MemberRoleConverter
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "members")
+class Member(
+    @Id
+    @Column(name = "member_id")
+    val id: Long = 0,
+
+    @Column(name = "member_name", nullable = false)
+    var name: String,
+
+    @Column(name = "email", unique = true)
+    var email: String? = null,
+
+    @Column(name = "phone_number", unique = true)
+    var phoneNumber: String? = null,
+
+    @Convert(converter = MemberRoleConverter::class)
+    @Column(name = "member_role", nullable = false, length = 20)
+    var role: MemberRole = MemberRole.BUYER,
+
+    @OneToMany(mappedBy = "member", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val socialAccounts: MutableList<SocialAccount> = mutableListOf(),
+
+    @Version
+    @Column(name = "row_version")
+    val version: Long? = null
+) : AbstractTime()
