37fb5bfc337422a5d35537c01d2bf5be30ef89ed
codrin2 <hssang127@gmail.com>
2025-07-28 17:55:27 +0900

    feat: 스타일 공유 기능을 위한 Look 엔티티 추가 #1

---
+++ src/main/kotlin/com/dh/baro/look/domain/Look.kt
@@ -0,0 +1,38 @@
+package com.dh.baro.look.domain
+
+import com.dh.baro.core.AbstractTime
+import com.dh.baro.identity.domain.Member
+import com.dh.baro.product.domain.Product
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "looks")
+class Look(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "look_id")
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id")
+    val creator: Member? = null,
+
+    @Column(name = "title", nullable = false)
+    var title: String,
+
+    @Lob @Column(name = "description")
+    var description: String? = null,
+
+    @Column(name = "likes_count", nullable = false)
+    var likesCount: Int = 0,
+
+    @OneToMany(mappedBy = "look", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val images: MutableList<LookImage> = mutableListOf(),
+
+    @ManyToMany
+    @JoinTable(
+        name = "look_products",
+        joinColumns = [JoinColumn(name = "look_id")],
+        inverseJoinColumns = [JoinColumn(name = "product_id")]
+    )
+    val products: MutableSet<Product> = mutableSetOf()
+) : AbstractTime()
