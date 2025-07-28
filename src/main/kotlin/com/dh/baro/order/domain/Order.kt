cdddb40fd1c2426493a4c00764868676f5bd28ee
codrin2 <hssang127@gmail.com>
2025-07-28 17:59:11 +0900

    feat: 주문 정보를 저장하는 Order 엔티티 추가 #1

---
+++ src/main/kotlin/com/dh/baro/order/domain/Order.kt
@@ -0,0 +1,31 @@
+package com.dh.baro.order.domain
+
+import com.dh.baro.core.AbstractTime
+import com.dh.baro.core.converter.OrderStatusConverter
+import com.dh.baro.identity.domain.Member
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "orders")
+class Order(
+    @Id
+    @Column(name = "order_id")
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member,
+
+    @Column(name = "total_price", nullable = false)
+    var totalPrice: Int,
+
+    @Column(name = "shipping_address", nullable = false, length = 500)
+    var shippingAddress: String,
+
+    @Convert(converter = OrderStatusConverter::class)
+    @Column(name = "status", nullable = false, length = 20)
+    var status: OrderStatus = OrderStatus.ORDERED,
+
+    @OneToMany(mappedBy = "order", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val items: MutableList<OrderItem> = mutableListOf()
+) : AbstractTime()
