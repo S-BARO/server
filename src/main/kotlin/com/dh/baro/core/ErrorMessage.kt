9c38a63ab1e6ca8bde5d14087da52773978d6bc6
codrin2 <hssang127@gmail.com>
2025-07-30 15:51:29 +0900

    refactor: ErrorMessage enum 확장 및 메시지 포맷 기능 추가 #3

--- src/main/kotlin/com/dh/baro/core/ErrorMessage.kt
+++ src/main/kotlin/com/dh/baro/core/ErrorMessage.kt
@@ -1,8 +1,16 @@
 package com.dh.baro.core
 
 enum class ErrorMessage(val message: String) {
-    UNHANDLED_EXCEPTION("UNHANDLED EXCEPTION"),
-    NOT_FOUND("NOT FOUND"),
-    UNAUTHORIZED("Unauthorized request"),
-    BAD_REQUEST("Bad request"),
+
+    // Common
+    UNHANDLED_EXCEPTION("서버 오류가 발생했습니다. 관리자에게 문의해주세요."),
+    NO_RESOURCE_FOUND("요청한 리소스를 찾을 수 없습니다."),
+
+    // Identity
+    UNSUPPORTED_SOCIAL_LOGIN("지원하지 않는 소셜 로그인 제공자입니다: %s"),
+    KAKAO_USER_FETCH_FAILED("카카오 사용자 정보 조회 실패: %s"),
+    ;
+
+    fun format(vararg args: Any): String =
+        message.format(*args)
 }
