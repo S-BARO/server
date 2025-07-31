32f5bc4b1024c498892d5b335d821c2fca5f4ec3
codrin2 <hssang127@gmail.com>
2025-07-31 11:44:31 +0900

    fix: 응답값으로 member role 추가 #5

--- src/main/kotlin/com/dh/baro/identity/application/dto/AuthResult.kt
+++ src/main/kotlin/com/dh/baro/identity/application/dto/AuthResult.kt
@@ -2,5 +2,6 @@ package com.dh.baro.identity.application.dto
 
 data class AuthResult(
     val memberId: Long,
+    val memberRole: String,
     val isNew: Boolean,
 )
--- src/main/kotlin/com/dh/baro/identity/domain/dto/RegistrationResult.kt
+++ src/main/kotlin/com/dh/baro/identity/domain/dto/RegistrationResult.kt
@@ -2,5 +2,6 @@ package com.dh.baro.identity.domain.dto
 
 data class RegistrationResult(
     val memberId: Long,
+    val memberRole: String,
     val isNew: Boolean,
 )
