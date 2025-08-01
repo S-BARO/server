97cd111240f6573049512d19f3749e3e4c131d42
codrin2 <hssang127@gmail.com>
2025-08-01 15:47:03 +0900

    feat: 장바구니 수량 증가 및 변경 기능 추가 #7

--- src/main/kotlin/com/dh/baro/cart/domain/CartItem.kt
+++ src/main/kotlin/com/dh/baro/cart/domain/CartItem.kt
@@ -1,14 +1,15 @@
 package com.dh.baro.cart.domain
 
 import com.dh.baro.core.AbstractTime
-import com.dh.baro.identity.domain.Member
+import com.dh.baro.core.IdGenerator
+import com.dh.baro.identity.domain.User
 import com.dh.baro.product.domain.Product
 import jakarta.persistence.*
 
 @Entity
 @Table(
     name = "cart_items",
-    uniqueConstraints = [UniqueConstraint(columnNames = ["member_id", "product_id"])]
+    uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "product_id"])]
 )
 class CartItem(
     @Id
@@ -16,8 +17,8 @@ class CartItem(
     val id: Long,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    val member: Member,
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
@@ -25,4 +26,23 @@ class CartItem(
 
     @Column(name = "quantity", nullable = false)
     var quantity: Int = 1
-) : AbstractTime()
+) : AbstractTime() {
+
+    fun addQuantity(quantity: Int) {
+        this.quantity += quantity
+    }
+
+    fun changeQuantity(quantity: Int) {
+        this.quantity = quantity
+    }
+
+    companion object {
+        fun newCartItem(user: User, product: Product): CartItem {
+            return CartItem(
+                id = IdGenerator.generate(),
+                user = user,
+                product = product,
+            )
+        }
+    }
+}
