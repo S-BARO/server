cd46d8c63b1ccd06da3cd1bd4243204d20140ec0
codrin2 <hssang127@gmail.com>
2025-08-03 20:23:06 +0900

    feat: CategoryService에 카테고리 생성 및 ID 존재 검증 기능 추가 #9

---
+++ src/main/kotlin/com/dh/baro/product/domain/CategoryService.kt
@@ -0,0 +1,27 @@
+package com.dh.baro.product.domain
+
+import com.dh.baro.core.ErrorMessage
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class CategoryService(
+    private val categoryRepository: CategoryRepository,
+) {
+
+    @Transactional
+    fun createCategory(id: Long, name: String): Category {
+        if (categoryRepository.existsById(id))
+            throw IllegalArgumentException(ErrorMessage.CATEGORY_ALREADY_EXISTS.format(id))
+
+        return categoryRepository.save(Category(id, name))
+    }
+
+    fun checkCategoryIdsExist(ids: List<Long>): List<Category> {
+        val categories = categoryRepository.findAllById(ids).toList()
+        if (categories.size != ids.size)
+            throw IllegalArgumentException(ErrorMessage.CATEGORY_NOT_FOUND.format(ids))
+        return categories
+    }
+}
