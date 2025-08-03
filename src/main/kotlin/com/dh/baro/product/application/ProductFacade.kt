4518ec53c50c33fcd966ab6d763e0b4582b0b7e4
codrin2 <hssang127@gmail.com>
2025-08-03 20:25:04 +0900

    feat: ProductFacade에 상품 등록 기능 구현 #9

---
+++ src/main/kotlin/com/dh/baro/product/application/ProductFacade.kt
@@ -0,0 +1,20 @@
+package com.dh.baro.product.application
+
+import com.dh.baro.product.domain.CategoryService
+import com.dh.baro.product.domain.Product
+import com.dh.baro.product.domain.ProductService
+import com.dh.baro.product.presentation.dto.ProductCreateRequest
+import org.springframework.stereotype.Service
+
+@Service
+class ProductFacade(
+    private val productService: ProductService,
+    private val categoryService: CategoryService,
+) {
+
+    fun createProduct(request: ProductCreateRequest): Product {
+        val categories = categoryService.checkCategoryIdsExist(request.categoryIds)
+        val cmd = ProductCreateCommand.toCommand(request)
+        return productService.createProduct(cmd, categories)
+    }
+}
