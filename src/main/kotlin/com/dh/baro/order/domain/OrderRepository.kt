3bb8a5fd0b01cdb7b245c2e1f1a3a887ce2da306
codrin2 <hssang127@gmail.com>
2025-08-05 17:09:18 +0900

    feat(OrderRepository): 주문 커서 기반 조회 및 상세 조회용 메서드 추가 #12

---
+++ src/main/kotlin/com/dh/baro/order/domain/OrderRepository.kt
@@ -0,0 +1,27 @@
+package com.dh.baro.order.domain
+
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface OrderRepository : JpaRepository<Order, Long> {
+
+    @Query("""
+        select o
+        from Order o
+        where o.user.id = :userId
+          and (:cursorId is null or o.id < :cursorId)
+        order by o.id desc
+    """)
+    fun findByUserIdAndCursorId(
+        @Param("userId")  userId: Long,
+        @Param("cursorId") cursorId: Long?,
+        pageable: Pageable,
+    ): Slice<Order>
+
+    @EntityGraph(attributePaths = ["items", "items.product"])
+    fun findOrderById(orderId: Long): Order?
+}
