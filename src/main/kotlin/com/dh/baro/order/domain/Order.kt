a9fb9d15aac2a6f62e67ec253549a2b00dda93ce
codrin2 <hssang127@gmail.com>
2025-08-05 17:02:14 +0900

    refactor(order): Order 도메인에 생성 로직 및 가격 계산 로직 추가 #12

--- src/main/kotlin/com/dh/baro/order/domain/Order.kt
+++ src/main/kotlin/com/dh/baro/order/domain/Order.kt
@@ -1,8 +1,11 @@
 package com.dh.baro.order.domain
 
 import com.dh.baro.core.AbstractTime
+import com.dh.baro.core.IdGenerator
 import com.dh.baro.identity.domain.User
 import jakarta.persistence.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 @Entity
 @Table(name = "orders")
@@ -16,7 +19,7 @@ class Order(
     val user: User,
 
     @Column(name = "total_price", nullable = false)
-    var totalPrice: Int,
+    var totalPrice: BigDecimal,
 
     @Column(name = "shipping_address", nullable = false, length = 500)
     var shippingAddress: String,
@@ -26,5 +29,30 @@ class Order(
     var status: OrderStatus = OrderStatus.ORDERED,
 
     @OneToMany(mappedBy = "order", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val items: MutableList<OrderItem> = mutableListOf()
-) : AbstractTime()
+    val items: MutableSet<OrderItem> = mutableSetOf()
+) : AbstractTime() {
+
+    fun addItem(item: OrderItem) =
+        items.add(item)
+
+    fun updateTotalPrice() {
+        totalPrice = items
+            .map { it.subTotal() }
+            .reduceOrNull(BigDecimal::add) ?: BigDecimal.ZERO
+            .setScale(0, RoundingMode.HALF_UP)
+    }
+
+    companion object {
+        fun newOrder(
+            user: User,
+            shippingAddress: String
+        ): Order =
+            Order(
+                id = IdGenerator.generate(),
+                user = user,
+                totalPrice = BigDecimal.ZERO,
+                shippingAddress = shippingAddress,
+                status = OrderStatus.ORDERED,
+            )
+    }
+}
