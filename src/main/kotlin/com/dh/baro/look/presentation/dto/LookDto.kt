04527c4bc4abbe551a1f5f3331e59b2e3962f43b
codrin2 <hssang127@gmail.com>
2025-08-12 14:46:35 +0900

    feat: LookDto DTO 추가 #16

---
+++ src/main/kotlin/com/dh/baro/look/presentation/dto/LookDto.kt
@@ -0,0 +1,17 @@
+package com.dh.baro.look.presentation.dto
+
+import com.dh.baro.look.domain.Look
+
+data class LookDto(
+    val id: Long,
+    val title: String,
+    val thumbnailUrl: String,
+) {
+    companion object {
+        fun from(look: Look) = LookDto(
+            id = look.id,
+            title = look.title,
+            thumbnailUrl = look.thumbnailUrl,
+        )
+    }
+}
