4c2da781fa890ff451eb412d55336a4b5dce8865
codrin2 <hssang127@gmail.com>
2025-08-12 13:40:49 +0900

    feat: LookReaction 엔티티 추가 #16

---
+++ src/main/kotlin/com/dh/baro/look/domain/LookReaction.kt
@@ -0,0 +1,41 @@
+package com.dh.baro.look.domain
+
+import com.dh.baro.core.AbstractTime
+import com.dh.baro.core.IdGenerator
+import jakarta.persistence.*
+
+@Entity
+@Table(
+    name = "look_reactions",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "look_id"])],
+)
+class LookReaction(
+    @Id
+    @Column(name = "id")
+    val id: Long,
+
+    @Column(name = "user_id", nullable = false)
+    val userId: Long,
+
+    @Column(name = "look_id", nullable = false)
+    val lookId: Long,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "reaction_type", nullable = false, length = 10)
+    var reactionType: ReactionType,
+) : AbstractTime() {
+
+    companion object {
+        fun of(
+            userId: Long,
+            lookId: Long,
+            reactionType: ReactionType
+        ) =
+            LookReaction(
+                id = IdGenerator.generate(),
+                userId = userId,
+                lookId = lookId,
+                reactionType = reactionType,
+            )
+    }
+}
