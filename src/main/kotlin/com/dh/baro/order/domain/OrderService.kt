2767d9b55355b5d68bbb5f0c757d3006bf4c5500
codrin2 <hssang127@gmail.com>
2025-08-22 20:52:50 +0900

    feat: OrderService에 재고 차감 방식 개선한 createOrderV2 로직 추가 #25

--- src/main/kotlin/com/dh/baro/order/domain/OrderService.kt
+++ src/main/kotlin/com/dh/baro/order/domain/OrderService.kt
@@ -3,22 +3,32 @@ package com.dh.baro.order.domain
 import com.dh.baro.core.ErrorMessage
 import com.dh.baro.order.application.OrderCreateCommand
 import com.dh.baro.product.domain.repository.ProductRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 class OrderService(
     private val productRepository: ProductRepository,
     private val orderRepository: OrderRepository,
 ) {
 
-    @Transactional
     fun createOrder(cmd: OrderCreateCommand): Order {
         val order = Order.newOrder(cmd.userId, cmd.shippingAddress)
 
         val mergedItems = mergeDuplicateItems(cmd.items)
         mergedItems.forEach { addItemsToOrder(it, order) }
+        order.updateTotalPrice()
+
+        return orderRepository.save(order)
+    }
+
+    fun createOrderV2(cmd: OrderCreateCommand): Order {
+        val order = Order.newOrder(cmd.userId, cmd.shippingAddress)
+
+        val mergedItems = mergeDuplicateItems(cmd.items)
+        mergedItems.forEach { addItemsToOrderV2(it, order) }
         order.updateTotalPrice()
 
         return orderRepository.save(order)
@@ -39,6 +49,20 @@ class OrderService(
             ?: throw IllegalArgumentException(ErrorMessage.PRODUCT_NOT_FOUND.format(item.productId))
 
         product.deductStockForOrder(item.quantity)
+
+        val orderItem = OrderItem.newOrderItem(
+            order = order,
+            product = product,
+            quantity = item.quantity,
+        )
+        order.addItem(orderItem)
+    }
+
+    private fun addItemsToOrderV2(item: OrderCreateCommand.Item, order: Order) {
+        productRepository.deductStock(item.productId, item.quantity)
+
+        val product = productRepository.findByIdOrNull(item.productId)
+            ?: throw IllegalArgumentException(ErrorMessage.PRODUCT_NOT_FOUND.format(item.productId))
 
         val orderItem = OrderItem.newOrderItem(
             order = order,
