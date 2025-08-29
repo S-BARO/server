548c800c33ab88cf3db296f7531a4463003db296
codrin2 <hssang127@gmail.com>
2025-08-29 17:35:17 +0900

    refactor: OrderService에서 OrderCreateCommand 구조 변경 반영 #29

--- src/main/kotlin/com/dh/baro/order/domain/OrderService.kt
+++ src/main/kotlin/com/dh/baro/order/domain/OrderService.kt
@@ -3,7 +3,6 @@ package com.dh.baro.order.domain
 import com.dh.baro.core.ErrorMessage
 import com.dh.baro.core.exception.ConflictException
 import com.dh.baro.order.application.OrderCreateCommand
-import com.dh.baro.product.domain.Product
 import com.dh.baro.product.domain.repository.ProductRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,9 +16,9 @@ class OrderService(
 
     fun createOrder(cmd: OrderCreateCommand): Order {
         val order = Order.newOrder(cmd.userId, cmd.shippingAddress)
-        val productMapById = cmd.productList.associateBy { it.id }
+        val productMapById = cmd.orderItems.associateBy { it.product.id }
 
-        val mergedItems = mergeDuplicateItems(cmd.items)
+        val mergedItems = mergeDuplicateItems(cmd.orderItems)
         mergedItems.forEach { addItemsToOrder(it, order, productMapById) }
         order.updateTotalPrice()
 
@@ -27,28 +26,28 @@ class OrderService(
     }
 
     private fun mergeDuplicateItems(
-        items: List<OrderCreateCommand.Item>
-    ): List<OrderCreateCommand.Item> =
+        items: List<OrderCreateCommand.OrderItem>
+    ): List<OrderCreateCommand.OrderItem> =
         items
-            .groupBy { it.productId }
-            .map { (productId, group) ->
+            .groupBy { it.product.id }
+            .map { (_, group) ->
+                val firstItem = group.first()
                 val totalQty = group.sumOf { it.quantity }
-                OrderCreateCommand.Item(productId, totalQty)
+                OrderCreateCommand.OrderItem(firstItem.product, totalQty)
             }
 
-    private fun addItemsToOrder(item: OrderCreateCommand.Item, order: Order, productMapById: Map<Long, Product>) {
-        val product = productMapById[item.productId]
-            ?: throw IllegalArgumentException(ErrorMessage.PRODUCT_NOT_FOUND.format(item.productId))
+    private fun addItemsToOrder(item: OrderCreateCommand.OrderItem, order: Order, productMapById: Map<Long, OrderCreateCommand.OrderItem>) {
+        val product = item.product
 
-        val updated = productRepository.deductStock(item.productId, item.quantity)
+        val updated = productRepository.deductStock(item.product.id, item.quantity)
 
         require(updated != 0) {
-            throw ConflictException(ErrorMessage.OUT_OF_STOCK.format(item.productId))
+            throw ConflictException(ErrorMessage.OUT_OF_STOCK.format(item.product.id))
         }
 
         val orderItem = OrderItem.newOrderItem(
             order = order,
-            productId = item.productId,
+            productId = item.product.id,
             name = product.getName(),
             thumbnailUrl = product.getThumbnailUrl(),
             quantity = item.quantity,
