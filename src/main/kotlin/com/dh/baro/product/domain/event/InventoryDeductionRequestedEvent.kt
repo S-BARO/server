8f3cfd4f477473d910aff34019e29a0e02214d39
codrin2 <hssang127@gmail.com>
2025-08-30 21:34:18 +0900

    refactor: 형식 변경 #29

--- src/main/kotlin/com/dh/baro/product/domain/event/InventoryDeductionRequestedEvent.kt
+++ src/main/kotlin/com/dh/baro/product/domain/event/InventoryDeductionRequestedEvent.kt
@@ -3,10 +3,10 @@ package com.dh.baro.product.domain.event
 data class InventoryDeductionRequestedEvent(
     val orderId: Long,
     val userId: Long,
-    val items: List<InventoryItem>
+    val items: List<InventoryItem>,
 ) {
     data class InventoryItem(
         val productId: Long,
-        val quantity: Int
+        val quantity: Int,
     )
 }
