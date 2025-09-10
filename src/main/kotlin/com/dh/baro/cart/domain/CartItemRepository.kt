44cfa155765716d401a2d942c9c78fcfa90175d9
codrin2 <hssang127@gmail.com>
2025-09-10 09:46:49 +0900

    refactor: CartItemRepository에서 불필요한 EntityGraph 제거 #35

--- src/main/kotlin/com/dh/baro/cart/domain/CartItemRepository.kt
+++ src/main/kotlin/com/dh/baro/cart/domain/CartItemRepository.kt
@@ -1,11 +1,9 @@
 package com.dh.baro.cart.domain
 
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CartItemRepository : JpaRepository<CartItem, Long> {
 
-    @EntityGraph(attributePaths = ["product"])
     fun findByUserId(userId: Long): List<CartItem>
 
     fun findByUserIdAndProductId(userId: Long, productId: Long): CartItem?
