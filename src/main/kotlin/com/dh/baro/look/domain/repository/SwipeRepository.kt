a2150231150a94d0ebcb1b4d03077a3941fde108
codrin2 <hssang127@gmail.com>
2025-09-11 21:24:51 +0900

    feat: SwipeRepository 도메인 인터페이스 정의 #37

---
+++ src/main/kotlin/com/dh/baro/look/domain/repository/SwipeRepository.kt
@@ -0,0 +1,18 @@
+package com.dh.baro.look.domain.repository
+
+import com.dh.baro.look.domain.Swipe
+
+interface SwipeRepository {
+
+    fun save(swipe: Swipe): Swipe
+
+    fun existsByUserIdAndLookId(userId: Long, lookId: Long): Boolean
+
+    fun findByUserIdAndLookId(userId: Long, lookId: Long): Swipe?
+
+    fun findUserSwipeHistory(userId: Long): List<Swipe>
+
+    fun deleteByUserIdAndLookId(userId: Long, lookId: Long): Long
+
+    fun deleteLikeIfExists(userId: Long, lookId: Long): Int
+}
