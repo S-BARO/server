6baf317b32fe65eee9db2fb40423e0a6b7953200
codrin2 <hssang127@gmail.com>
2025-09-12 16:47:12 +0900

    chore: mongodb uri config로 설정 #37

--- src/main/kotlin/com/dh/baro/core/config/MongoConfig.kt
+++ src/main/kotlin/com/dh/baro/core/config/MongoConfig.kt
@@ -1,8 +1,10 @@
 package com.dh.baro.core.config
 
+import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -12,9 +14,13 @@ import java.util.concurrent.TimeUnit
 @EnableMongoAuditing
 class MongoConfig {
 
+    @Value("\${spring.data.mongodb.uri}")
+    private lateinit var mongoUri: String
+
     @Bean
     fun mongoClient(): MongoClient {
         val settings = MongoClientSettings.builder()
+            .applyConnectionString(ConnectionString(mongoUri))
             .applyToConnectionPoolSettings { builder ->
                 builder.maxSize(30)
                     .minSize(10)
