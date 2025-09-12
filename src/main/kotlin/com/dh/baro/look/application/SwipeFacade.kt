b6074f5b1e6fae5388acbf7a567b7e8d73e139b1
codrin2 <hssang127@gmail.com>
2025-09-13 00:31:28 +0900

    refactor: 불필요한 @Transactional 제거

--- src/main/kotlin/com/dh/baro/look/application/SwipeFacade.kt
+++ src/main/kotlin/com/dh/baro/look/application/SwipeFacade.kt
@@ -3,14 +3,12 @@ package com.dh.baro.look.application
 import com.dh.baro.look.domain.ReactionType
 import com.dh.baro.look.domain.service.SwipeService
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SwipeFacade(
     private val swipeService: SwipeService,
 ) {
 
-    @Transactional
     fun recordSwipe(userId: Long, lookId: Long, reactionType: ReactionType) {
         swipeService.upsertSwipe(
             userId = userId,
