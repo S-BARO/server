f863d4c7c54db134663a8458673bfebfa2f10696
codrin2 <hssang127@gmail.com>
2025-09-16 19:35:58 +0900

    feat: 불필요한 변수 및 클래스 제거 #40

--- src/main/kotlin/com/dh/baro/core/outbox/OutboxMessageRouter.kt
+++ src/main/kotlin/com/dh/baro/core/outbox/OutboxMessageRouter.kt
@@ -3,25 +3,18 @@ package com.dh.baro.core.outbox
 import com.dh.baro.core.ErrorMessage
 import com.dh.baro.core.config.KafkaConfig
 import com.dh.baro.order.domain.event.OrderPlacedEvent
-import com.dh.baro.product.infra.event.InventoryDeductionRequestedEvent
-import com.dh.baro.product.infra.event.InventoryEventHandler
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
 class OutboxMessageRouter(
-    private val inventoryEventHandler: InventoryEventHandler,
     private val kafkaTemplate: KafkaTemplate<String, Any>,
     private val objectMapper: ObjectMapper,
 ) {
 
     fun route(msg: OutboxMessage) {
         when (msg.eventType) {
-            INVENTORY_DEDUCTION_EVENT -> {
-                val event = objectMapper.readValue(msg.payload, InventoryDeductionRequestedEvent::class.java)
-                inventoryEventHandler.handleEvent(event)
-            }
             ORDER_PLACED_EVENT -> {
                 val event = objectMapper.readValue(msg.payload, OrderPlacedEvent::class.java)
                 kafkaTemplate.send(KafkaConfig.ORDER_EVENTS_TOPIC, event.orderId.toString(), event)
@@ -32,7 +25,6 @@ class OutboxMessageRouter(
     }
 
     companion object {
-        const val INVENTORY_DEDUCTION_EVENT = "INVENTORY_DEDUCTION_REQUESTED"
         const val ORDER_PLACED_EVENT = "ORDER_PLACED"
     }
 }
--- src/main/kotlin/com/dh/baro/product/infra/event/InventoryDeductionRequestedEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.dh.baro.product.infra.event
-
-import com.dh.baro.product.domain.InventoryItem
-
-data class InventoryDeductionRequestedEvent(
-    val orderId: Long,
-    val userId: Long,
-    val items: List<InventoryItem>,
-)
