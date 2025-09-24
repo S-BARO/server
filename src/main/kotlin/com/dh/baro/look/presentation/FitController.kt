96e56ee8c60e26b9023e1147ccf04ed1a522bb1a
codrin2 <hssang127@gmail.com>
2025-09-24 17:47:17 +0900

    feat: FitController에 피팅 이미지 소스 관련 API 추가 #43

---
+++ src/main/kotlin/com/dh/baro/look/presentation/FitController.kt
@@ -0,0 +1,40 @@
+package com.dh.baro.look.presentation
+
+import com.dh.baro.core.annotation.CurrentUser
+import com.dh.baro.look.application.FittingSourceImageFacade
+import com.dh.baro.look.presentation.dto.*
+import com.dh.baro.look.presentation.swagger.FitSwagger
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/fit")
+class FitController(
+    private val fittingSourceImageFacade: FittingSourceImageFacade,
+) : FitSwagger {
+
+    @PostMapping("/source-images/upload-url")
+    @ResponseStatus(HttpStatus.OK)
+    override fun generateUploadUrl(
+        @CurrentUser userId: Long,
+    ): FittingSourceImageUploadUrlResponse =
+        FittingSourceImageUploadUrlResponse.from(
+            fittingSourceImageFacade.generateUploadUrl(userId)
+        )
+
+    @PutMapping("/source-images/{imageId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    override fun completeImageUpload(
+        @CurrentUser userId: Long,
+        @PathVariable imageId: Long,
+    ) = fittingSourceImageFacade.completeImageUpload(imageId, userId)
+
+    @GetMapping("/source-images")
+    @ResponseStatus(HttpStatus.OK)
+    override fun getUserFittingSourceImages(
+        @CurrentUser userId: Long,
+    ): FittingSourceImageListResponse =
+        FittingSourceImageListResponse.from(
+            fittingSourceImageFacade.getUserFittingSourceImages(userId)
+        )
+}
