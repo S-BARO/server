1a02e2a25cd6bed9a9283e8c646ebea47991ef6e
codrin2 <hssang127@gmail.com>
2025-09-30 15:52:39 +0900

    fix: presignedURL 생성 시 별도 스레드에서 실행하여 요청 컨텍스트 격리하도록 처리

--- src/main/kotlin/com/dh/baro/look/infra/s3/S3ImageApi.kt
+++ src/main/kotlin/com/dh/baro/look/infra/s3/S3ImageApi.kt
@@ -10,6 +10,8 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
 import java.time.Duration
 import java.time.Instant
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 @Component
 class S3ImageApi(
@@ -23,39 +25,46 @@ class S3ImageApi(
     val secretKey: String,
 ) {
 
+    private val isolatedExecutor = Executors.newCachedThreadPool()
+
     fun generatePresignedUrl(s3Key: String): S3PresignedUrlInfo {
-        val duration = Duration.ofMinutes(UPLOAD_DURATION_MINUTES)
-        val expiresAt = Instant.now().plus(duration)
+        // 별도 스레드에서 실행하여 요청 컨텍스트 격리
+        return CompletableFuture.supplyAsync({
+            val duration = Duration.ofMinutes(UPLOAD_DURATION_MINUTES)
+            val expiresAt = Instant.now().plus(duration)
 
-        val credentials = AwsBasicCredentials.create(accessKey, secretKey)
+            val credentials = AwsBasicCredentials.create(accessKey, secretKey)
 
-        val s3Presigner = S3Presigner.builder()
-            .region(Region.of(region))
-            .credentialsProvider(StaticCredentialsProvider.create(credentials))
-            .build()
+            val s3Presigner = S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build()
 
-        val putObjectRequest = PutObjectRequest.builder()
-            .bucket(bucketName)
-            .key(s3Key)
-            .build()
+            try {
+                val putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(s3Key)
+                    .build()
 
-        val presignRequest = PutObjectPresignRequest.builder()
-            .putObjectRequest(putObjectRequest)
-            .signatureDuration(duration)
-            .build()
+                val presignRequest = PutObjectPresignRequest.builder()
+                    .putObjectRequest(putObjectRequest)
+                    .signatureDuration(duration)
+                    .build()
 
-        val presignedRequest = s3Presigner.presignPutObject(presignRequest)
+                val presignedRequest = s3Presigner.presignPutObject(presignRequest)
 
-        s3Presigner.close()
-
-        return S3PresignedUrlInfo(
-            presignedUrl = presignedRequest.url().toString(),
-            s3Key = s3Key,
-            imageUrl = "https://$bucketName.s3.$region.amazonaws.com/$s3Key",
-            expiresAt = expiresAt,
-            maxFileSize = MAX_FILE_SIZE_BYTES,
-            allowedTypes = ALLOWED_CONTENT_TYPES,
-        )
+                S3PresignedUrlInfo(
+                    presignedUrl = presignedRequest.url().toString(),
+                    s3Key = s3Key,
+                    imageUrl = "https://$bucketName.s3.$region.amazonaws.com/$s3Key",
+                    expiresAt = expiresAt,
+                    maxFileSize = MAX_FILE_SIZE_BYTES,
+                    allowedTypes = ALLOWED_CONTENT_TYPES,
+                )
+            } finally {
+                s3Presigner.close()
+            }
+        }, isolatedExecutor).get()
     }
 
     fun getImageUrl(s3Key: String): String {
