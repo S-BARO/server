e3244b924c9ab0f2b9bc929fc41b344fe767dddb
codrin2 <hssang127@gmail.com>
2025-12-07 02:47:06 +0900

    fix: 좋아요 한 룩 조회 시 응답 ID String 변환 로직 추가

--- src/main/kotlin/com/dh/baro/look/domain/dto/LikedLookDto.kt
+++ src/main/kotlin/com/dh/baro/look/domain/dto/LikedLookDto.kt
@@ -1,7 +1,12 @@
 package com.dh.baro.look.domain.dto
 
+import com.dh.baro.core.serialization.LongToStringSerializer
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
 data class LikedLookDto(
+    @JsonSerialize(using = LongToStringSerializer::class)
     val lookReactionId: Long,
+    @JsonSerialize(using = LongToStringSerializer::class)
     val lookId: Long,
     val title: String,
     val thumbnailUrl: String,
